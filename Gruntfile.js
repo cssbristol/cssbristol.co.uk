4ef1cf03939517cf2435f60b4e0f3e543d8df357
Julian Laval <hello@julianlaval.com>
2015-11-15 16:21:43 +0000

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,283 @@
+// Generated on 2015-10-30 using
+// generator-webapp 1.1.0
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Automatically load required grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Configurable paths
+  var config = {
+    app: 'src',
+    dist: 'dist'
+  };
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    // Project settings
+    config: config,
+
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      bower: {
+        files: ['bower.json'],
+        tasks: ['wiredep']
+      },
+      js: {
+        files: ['<%= config.app %>/scripts/**/*.js', 'Gruntfile.js'],
+        tasks: ['eslint']
+      },
+      styles: {
+        files: ['<%= config.app %>/styles/**/*.less'],
+        tasks: ['less', 'postcss']
+      }
+    },
+
+    browserSync: {
+      options: {
+        notify: false,
+        background: true,
+        watchOptions: {
+          ignored: ''
+        }
+      },
+      livereload: {
+        options: {
+          files: [
+            '<%= config.app %>/**/*.html',
+            '.tmp/styles/{,*/}*.css',
+            '<%= config.app %>/images/**/*',
+            '<%= config.app %>/scripts/**/*.js'
+          ],
+          port: 9000,
+          server: {
+            baseDir: ['.tmp', config.app],
+            routes: {
+              '/bower_components': './bower_components'
+            }
+          }
+        }
+      },
+      dist: {
+        options: {
+          background: false,
+          server: '<%= config.dist %>'
+        }
+      }
+    },
+
+    // Empties folders to start fresh
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= config.dist %>/*',
+            '!<%= config.dist %>/.git*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+
+    // Make sure code styles are up to par and there are no obvious mistakes
+    eslint: {
+      options: {
+        configFile: '.eslintrc'
+      },
+      target: [
+        'Gruntfile.js',
+        '<%= config.app %>/scripts/**/*.js',
+        '!<%= config.app %>/scripts/vendor/*'
+      ]
+    },
+
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          // Add vendor prefixed styles
+          require('autoprefixer')({
+            browsers: ['> 1%', 'last 4 versions']
+          })
+        ]
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/styles/',
+          src: ['**/*.css'],
+          dest: '.tmp/styles/'
+        }]
+      }
+    },
+
+    // Compile LESS to CSS
+    less: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/styles/',
+          src: ['**/*.less'],
+          dest: '.tmp/styles/',
+          ext: '.css'
+        }]
+      }
+    },
+
+    // Automatically inject Bower components into the HTML file
+    wiredep: {
+      app: {
+        src: ['<%= config.app %>/index.html'],
+        ignorePath: /^(\.\.\/)*\.\./
+      }
+    },
+
+    // Renames files for browser caching purposes
+    filerev: {
+      dist: {
+        src: [
+          '<%= config.dist %>/scripts/**/*.js',
+          '<%= config.dist %>/styles/**/*.css',
+          '<%= config.dist %>/images/**/*.*',
+          '<%= config.dist %>/fonts/**/*.*',
+          '<%= config.dist %>/*.{ico,png}'
+        ]
+      }
+    },
+
+    // Reads HTML for usemin blocks to enable smart builds that automatically
+    // concat, minify and revision files. Creates configurations in memory so
+    // additional tasks can operate on them
+    useminPrepare: {
+      options: {
+        dest: '<%= config.dist %>'
+      },
+      html: '<%= config.app %>/index.html'
+    },
+
+    // Performs rewrites based on rev and the useminPrepare configuration
+    usemin: {
+      options: {
+        assetsDirs: [
+          '<%= config.dist %>',
+          '<%= config.dist %>/images',
+          '<%= config.dist %>/styles'
+        ]
+      },
+      html: ['<%= config.dist %>/**/*.html'],
+      css: ['<%= config.dist %>/styles/**/*.css']
+    },
+
+    // The following *-min tasks produce minified files in the dist folder
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/images',
+          src: '**/*.{gif,jpeg,jpg,png}',
+          dest: '<%= config.dist %>/images'
+        }]
+      }
+    },
+
+    svgmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/images',
+          src: '**/*.svg',
+          dest: '<%= config.dist %>/images'
+        }]
+      }
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          removeAttributeQuotes: true,
+          removeCommentsFromCDATA: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true,
+          // true would impact styles with attribute selectors
+          removeRedundantAttributes: false,
+          useShortDoctype: true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= config.dist %>',
+          src: '**/*.html',
+          dest: '<%= config.dist %>'
+        }]
+      }
+    },
+
+    // Copies remaining files to places other tasks can use
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= config.app %>',
+          dest: '<%= config.dist %>',
+          src: [
+            '*.{ico,png,txt}',
+            'images/**/*.webp',
+            '**/*.html',
+            'README.md',
+            'fonts/**/*.*'
+          ]
+        }]
+      }
+      // styles: {
+      //   expand: true,
+      //   dot: true,
+      //   cwd: '<%= config.app %>/styles',
+      //   dest: '.tmp/styles/',
+      //   src: '**/*.css'
+      // }
+    }
+  });
+
+  grunt.registerTask('serve', 'start the server and preview your app', function (target) {
+
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'browserSync:dist']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'wiredep',
+      'less',
+      'postcss',
+      'browserSync:livereload',
+      'watch'
+    ]);
+  });
+
+  grunt.registerTask('build', [
+    'newer:eslint',
+    'clean:dist',
+    'wiredep',
+    'useminPrepare',
+    'less',
+    'imagemin',
+    'svgmin',
+    'postcss',
+    'concat',
+    'cssmin',
+    'uglify',
+    'copy:dist',
+    'filerev',
+    'usemin',
+    'htmlmin'
+  ]);
+
+  grunt.registerTask('default', ['build']);
+};
