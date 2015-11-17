d99a4a9eda65d788572d2b6f58772e04a4fa2d54
Julian Laval <hello@julianlaval.com>
2015-11-17 15:31:14 +0000

    Collapsible nav

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+// Particle Canvas setup
+
 var options = {
   particleColor: '#999',
   background: 'images/header-bg.jpg',
@@ -6,3 +8,26 @@ var options = {
   density: 'high'
 };
 var particleCanvas = new ParticleNetwork(document.getElementById('header'), options);
+
+// Menu icon event handler
+
+var navOpen = $('#nav-open');
+var navClose = $('#nav-close');
+var collapsedNav = $('#collapsed-header');
+
+navOpen.on('click', function () {
+  navOpen.fadeOut();
+  collapsedNav.fadeIn();
+});
+
+navClose.on('click', function () {
+  collapsedNav.fadeOut();
+  navOpen.fadeIn().css('display', '');
+});
+
+$(window).resize(function () {
+  if ($(window).width() > 950) {
+    navOpen.css('display', '');
+    collapsedNav.fadeOut();
+  }
+});
