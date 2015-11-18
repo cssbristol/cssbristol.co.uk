5631af94d730e74d7e37914f2a8613419a0fea8f
Julian Laval <hello@julianlaval.com>
2015-11-18 19:39:05 +0000

    Built from commit 263380b on branch develop

---
+++ scripts/about.b4d274cd.js
@@ -0,0 +1,1 @@
+var navOpen=$("#nav-open"),navClose=$("#nav-close"),collapsedNav=$("#collapsed-header");navOpen.on("click",function(){navOpen.fadeOut(),collapsedNav.fadeIn()}),navClose.on("click",function(){collapsedNav.fadeOut(),navOpen.fadeIn().css("display","")}),$(window).resize(function(){$(window).width()>950&&(navOpen.css("display",""),collapsedNav.fadeOut())});
