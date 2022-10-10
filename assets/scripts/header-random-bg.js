54a6240772cdabd69692f051d32c6d3215368b78
14ROVI <14ROVI@users.noreply.github.com>
2022-10-10 13:36:36 +0000

    deploy: ef4728db836de5ec59fa488a13a0bed2b0940b59

--- assets/scripts/header-random-bg.js
+++ assets/scripts/header-random-bg.js
@@ -1,34 +1,9 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".noscript").forEach(elem => {
         elem.classList.remove("noscript");
     });
 
-    const bg = document.querySelector('#page-header--random-bg .bg > div');
-    let images = [
-        'header-bg-01', 'header-bg-02', 'header-bg-03', 'header-bg-04',
-        'header-bg-01', 'header-bg-02', 'header-bg-03', 'header-bg-04',
-        'header-bg-01', 'header-bg-02', 'header-bg-03', 'header-bg-04',
-        'header-bg-01', 'header-bg-02', 'header-bg-03', 'header-bg-04',
-        'header-bg-01', 'header-bg-02', 'header-bg-03', 'header-bg-04',
-        'header-bg-01', 'header-bg-02', 'header-bg-03', 'header-bg-04',
-        'header-bg-01', 'header-bg-02', 'header-bg-03', 'header-bg-04',
-        'header-bg-01', 'header-bg-02', 'header-bg-03', 'header-bg-04',
-        'header-bg-01', 'header-bg-02', 'header-bg-03', 'header-bg-04',
-        'header-bg-01', 'header-bg-02', 'header-bg-03', 'header-bg-04',
-        'header-bg-01', 'header-bg-02', 'header-bg-03', 'header-bg-04',
-        'header-bg-01', 'header-bg-02', 'header-bg-03', 'header-bg-04',
-        'header-bg-05'
-    ];
-    
-    const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-    // Image 5 is special case--don't blur and don't use smaller version
-    let isImg5 = (chosenImage === "header-bg-05");
-
-    let filename;
-    if(isImg5) filename = "header-bg-05.jpg";
-    else filename = `${chosenImage}_70000.jpg`
-
-    bg.style.backgroundImage = `url(/assets/images/core/${filename})`;
-    if(!isImg5) bg.classList.add("blur");
+    const bg = document.getElementById("header-random-background");
+    const imageIndex = Math.floor(Math.random() * 4) + 1;
+    bg.style.backgroundImage = `url(/assets/images/core/header-bg-${imageIndex}_70000.jpg)`;
 });
