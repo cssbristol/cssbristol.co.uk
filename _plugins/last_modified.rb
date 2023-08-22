6a8c41420bac6ad24f0bf77e0027cd00e1441883
a6.webm <46289515+a6-webm@users.noreply.github.com>
2023-08-22 20:56:11 +0100

    typo

--- _plugins/last_modified.rb
+++ _plugins/last_modified.rb
@@ -3,8 +3,8 @@ module Jekyll
     module Hook
       def self.add_determinator_proc
         proc { |item|
-        absolute_path_to_article = Jekyll.sanitized_path(item.site.source, item.path)
-        git_log_time = `git --git-dir #{Dir.pwd + "/.git"} log -1 --format="%ct" #{absolute_path_to_article} || echo "0"`[..-2]
+          absolute_path_to_article = Jekyll.sanitized_path(item.site.source, item.path)
+          git_log_time = `git --git-dir #{Dir.pwd + "/.git"} log -1 --format="%ct" #{absolute_path_to_article} || echo "0"`[..-2]
           item.data['last_modified_at'] = Time.at(git_log_time.to_i())
         }
       end
